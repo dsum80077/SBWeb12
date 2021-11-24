425193528439969ab3c8d1a30c9b59861ab9d92e
ssanjay8 <ssanjay8@host.docker.internal>
2021-11-24 11:51:56 +0530

    First Commit

---
+++ SBWeb12/src/main/java/com/example/demo/SbWeb12Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbWeb12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbWeb12Application.class, args);
+	}
+
+}
