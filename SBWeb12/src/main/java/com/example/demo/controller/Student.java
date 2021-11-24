7a4bc7927d80f3daf03aa32c161c6ad2f9959f09
ssanjay8 <ssanjay8@host.docker.internal>
2021-11-24 11:57:37 +0530

    Student Class Updated

---
+++ SBWeb12/src/main/java/com/example/demo/controller/Student.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+public class Student {
+	private int roll;
+	private String sname;
+	private String address;
+	public int getRoll() {
+		return roll;
+	}
+	public void setRoll(int roll) {
+		this.roll = roll;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+
+}
